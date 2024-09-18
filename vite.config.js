d66060d364a79fd6eea19dfcfbf3a783b9b57535
kaviyarasu2224 <kaviyarasu2224@gmail.com>
2024-09-18 16:43:15 +0530

    updata

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   base: '/Personal-portfolio/', // This should match the repository name
+   base: '/Personal-web/', // This should match the repository name
   plugins: [react()],
 })
